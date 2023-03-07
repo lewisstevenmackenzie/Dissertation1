98b73aee8236fbf1f256cb454f03ad40b89ed2f3
lewisstevenmackenzie <64756559+lewisstevenmackenzie@users.noreply.github.com>
2023-03-07 18:00:56 +0000

    custom action for module querying

--- actions/actions.py
+++ actions/actions.py
@@ -24,7 +24,87 @@ class ActionRepeatValues(Action):
 
         msg = f"Okay, so you would like to study {studyField} and you are {codingAbilityEnglish[int(codingAbility)-1]} at coding. You also have said you are inerested in learning the {language} language. I will now search for some possible modules that fit this criteria."
 
-        #msg2 = f"This is the Study field: {studyField} \nThis is the value for the coding ability: {codingAbility} \nThis is the language: {language}"
+        dispatcher.utter_message(text=msg)
+
+        return [] 
+
+
+class ActionModuleSpecifications(Action):
+
+    def name(self) -> Text:
+        return "action_module_specifications"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        import os
+        import csv
+
+        cur_path = os.path.dirname(__file__)
+        file_path = os.path.join(cur_path, '..\\dataset\\moduleInfoTest.csv')
+        moduleName = tracker.get_slot("module")
+
+        print('this is the module name: ' + moduleName)
+        msg = ""
+
+        with open(file_path, newline='') as csvfile:
+        
+            reader = csv.DictReader(csvfile)
+        
+            for row in reader:
+                if (moduleName.lower() in row['Module Name'].lower()):
+                    print(row['Module Code'], row['Module Name'])
+                    
+                    msg = f"The {row['Module Name']} module is run by {row['Lecturer']}. Here is some more information regarding the module.\n"
+
+                    if (row['Coursework'] == "0"):
+                        msg = msg + "This module does not contain a coursework element. "
+                    else:
+                        msg = msg + f"The module is {row['Coursework']}% coursework. "
+                    
+                    if (row['Exam'] == "0"):
+                        msg = msg + "There is no exam element to this Module. "
+                    else:
+                        msg =msg + f"The exam portion of this module acounts for {row['Exam']}% of the final grade. "
+
+                    if row['Class Test'] == "0":
+                        msg = msg + "There will not be a class test for this module. "
+                    else: 
+                        msg = msg + f"The class test portion is {row['Class Test']}%. "
+                     
+                    msg =  msg +  f"The module is worth {row['Credits']} credits and is SCQF Level {row['SCQF Level']}\nModule Description:\n{row['Description']}"
+
+        dispatcher.utter_message(text=msg)
+
+        return [] 
+
+
+class ActionListModules(Action):
+
+    def name(self) -> Text:
+        return "action_list_modules"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        import os
+        import csv
+
+        cur_path = os.path.dirname(__file__)
+        file_path = os.path.join(cur_path, '..\\dataset\\moduleInfoTest.csv')
+
+        msg = f"This is a list of the modules available to you:\n"        
+
+        with open(file_path, newline='') as csvfile:
+        
+            reader = csv.DictReader(csvfile)
+        
+            for row in reader:
+                print(row['Module Code'], row['Module Name'])
+
+                msg = msg + f"  {row['Module Name']}\n"
 
         dispatcher.utter_message(text=msg)
 
